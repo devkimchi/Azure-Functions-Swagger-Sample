9641668d583181a22fa506cd55002126e6802106
Justin Yoo <justin.yoo@aliencube.com>
2017-06-12 19:28:09 +1000

    Add a new function to render Swagger definition

---
+++ src/SwaggerSample.FunctionApp/GetSwaggerHttpTrigger.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Threading.Tasks;
+
+using Microsoft.Azure.WebJobs.Host;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+using YamlDotNet.Serialization;
+
+namespace SwaggerSample.FunctionApp
+{
+    /// <summary>
+    /// This represents the HTTP trigger entity for the GetSwagger event.
+    /// </summary>
+    public static class GetSwaggerHttpTrigger
+    {
+        /// <summary>
+        /// Invokes the function.
+        /// </summary>
+        /// <param name="req"><see cref="HttpRequestMessage"/> instance.</param>
+        /// <param name="version">Swagger definition version.</param>
+        /// <param name="log"><see cref="TraceWriter"/> instance.</param>
+        /// <returns>Returns the <see cref="HttpResponseMessage"/> instance.</returns>
+        public static async Task<HttpResponseMessage> Run(HttpRequestMessage req, string version, TraceWriter log)
+        {
+            var filepath = $"swagger-{version}.yaml";
+            if (!File.Exists(filepath))
+            {
+                return req.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            var settings = new JsonSerializerSettings()
+                               {
+                                   ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                                   Formatting = Formatting.Indented
+                               };
+            var formatter = new JsonMediaTypeFormatter() { SerializerSettings = settings };
+
+            using (var reader = File.OpenText(filepath))
+            {
+                var yaml = await reader.ReadToEndAsync().ConfigureAwait(false);
+                var deserialiser = new DeserializerBuilder().Build();
+                var deserialised = deserialiser.Deserialize<dynamic>(yaml);
+
+                return req.CreateResponse(HttpStatusCode.OK, (object)deserialised, formatter);
+            }
+        }
+    }
+}
